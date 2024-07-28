c458f591fce45704ddc4c8367b078c2222955b03
SaiTeja1224 <saiteja1224@proton.me>
2024-07-28 19:30:43 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/topic-scouter",
+  base: "topic-scouter",
   plugins: [react()],
   resolve: {
     alias: {
